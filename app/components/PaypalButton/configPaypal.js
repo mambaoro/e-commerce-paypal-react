fced25e2bb0f6de5e85da6e111602224214f13a7
Baoro <mam.baoro@outlook.fr>
2019-07-23 03:46:44 +0200

    Fix

---
+++ app/components/PaypalButton/configPaypal.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+export const paypalConfig = {
+  PAYPAL_CLIENT_ID:
+    'Ac6MEmDd5ntGP9gECWVspdzubRifuMXAhnAM6ZfiF1_mRZ6lJMwQnmOYVLmsd01AFAecPsEWGrhVVeO_',
+};
